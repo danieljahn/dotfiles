0cfce96955fa2a139bff54cfe0956ebe66132120
Daniel Jahn <370305+danieljahn@users.noreply.github.com>
2021-02-10 22:28:18 +0100

    Setup homebrew completions

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -1,1 +1,7 @@
 export DOTFILES="$HOME/repos/dotfiles"
+
+# HOMEBREW
+eval $(/opt/homebrew/bin/brew shellenv)
+if type brew &>/dev/null; then
+  FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
+fi
